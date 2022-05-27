ad1a63f16add276cdba31d0d192c9c3f2844bcc2
Jeff Huijsmans <jeffhuys@gmail.com>
2022-05-27 20:58:18 +0200

    gcloud

--- packages/apps/my-app/src/main.ts
+++ packages/apps/my-app/src/main.ts
@@ -1,5 +1,9 @@
+import { http } from '@google-cloud/functions-framework'
 import {libsStoreChat} from "@cloud-func/libs/store-chat";
+import 'tslib'
 
-console.log('Hello World!');
+http('hello', (req, res) => {
+  const result = libsStoreChat();
+  res.send(result || 'Hello world!');
+})
 
-console.log(libsStoreChat())
