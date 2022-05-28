0fe4a8c927600f51c9e6c078c225a6a425d87882
Mauricio Kruijer <mauricio.kruijer@gmail.com>
2022-05-28 14:43:13 +0200

    Added telegraf

--- packages/apps/dolladollabillbot/src/main.ts
+++ packages/apps/dolladollabillbot/src/main.ts
@@ -1,13 +1,24 @@
-import * as express from 'express'
-import {libsStoreChat} from '@tandeytrader/libs/store-chat'
 import 'tslib'
+import * as express from 'express'
+import { Telegraf } from 'telegraf'
+import {libsStoreChat} from '@tandeytrader/libs/store-chat'
 
-const app = express();
+const { BOT_TOKEN, DEV } = process.env
+const version = 'v1-bot'
+const app = express()
 
-const { BOT_TOKEN, DEV } = process.env;
+if (BOT_TOKEN === undefined || BOT_TOKEN === '') {
+  throw new Error('BOT_TOKEN is required')
+}
+
+const bot = new Telegraf(BOT_TOKEN);
+
+bot.command('hello', (ctx) => {
+  ctx.reply('Hello, friend!')
+})
 
 app.get('/', async (req, res) => {
-  res.send('Your token is ' + BOT_TOKEN)
+  return res.send({ version })
 })
 
 app.post('/telegraf', async (req, res) => {
