e004f90b30038079f5a2087f634147d2c34deee4
Mauricio <mauricio.kruijer@gmail.com>
2022-05-28 20:03:57 +0200

    Add puppeteer and getChartUrl (#4)
    * Added puppeteer and getChartUrl feature

--- packages/apps/get-chart-url/src/main.ts
+++ packages/apps/get-chart-url/src/main.ts
@@ -1,4 +1,60 @@
-import { http } from '@google-cloud/functions-framework'
 import 'tslib'
+import { http } from '@google-cloud/functions-framework'
+import * as puppeteer from 'puppeteer'
 
-http('get-chart-url', (req, res) => res.send('ok!'))
+const getChartUrl = async (chartId: string) => {
+  const browser = await puppeteer.launch({
+    headless: true,
+    defaultViewport: {
+      width: 1920,
+      height: 1080
+    }
+  })
+
+  return new Promise( async (resolve, reject) => {
+    const timer = setTimeout(() => {
+      browser.close()
+      reject({error: 'Took too long to get url'})
+    }, 30 * 1000)
+
+    try {
+      const tradingview = await browser.newPage()
+      await tradingview.goto(`https://www.tradingview.com/chart/${chartId}/`, {
+        waitUntil: 'networkidle2',
+        timeout: 0
+      })
+
+      tradingview.on('popup', async(newTab) => {
+        await newTab.waitForSelector('main > img')
+        const url = await newTab.$eval('main > img', (image: HTMLImageElement) => image.src)
+
+        browser.close()
+        clearTimeout(timer)
+        resolve(url)
+      })
+
+      await tradingview.click('#header-toolbar-screenshot')
+      await tradingview.click('div[data-name="open-image-in-new-tab"]')
+    } catch (e) {
+      browser.close()
+      reject(e)
+    }
+  })
+}
+
+http('get-chart-url', async (req, res) => {
+  let data = {}
+  try {
+    if (req.method !== 'POST') {
+      const error = new Error('Only POST requests are accepted')
+      throw error
+    }
+
+    data = {url: await getChartUrl(req.body.chartId)}
+  } catch(e) {
+    console.log(e)
+    data = {error: 'something went wrong'}
+  } finally {
+    return res.status(200).send(data);
+  }
+})
