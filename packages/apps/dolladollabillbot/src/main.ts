d9097b8dea59e7cf655145e8daa5a686c0c97ad9
Mauricio <mauricio.kruijer@gmail.com>
2022-05-28 16:02:52 +0200

    Add telegraf (#3)
    * Feature telegram bot using telegraf

--- packages/apps/dolladollabillbot/src/main.ts
+++ packages/apps/dolladollabillbot/src/main.ts
@@ -1,17 +1,40 @@
-import * as express from 'express'
-import {libsStoreChat} from '@tandeytrader/libs/store-chat'
 import 'tslib'
+import * as express from 'express'
+import { Telegraf } from 'telegraf'
+import {libsStoreChat} from '@tandeytrader/libs/store-chat'
 
-const app = express();
+const { BOT_TOKEN, WEBHOOK_URL, DEV } = process.env
+const version = 'v1-bot'
+const app = express()
 
-const { BOT_TOKEN, DEV } = process.env;
+if (BOT_TOKEN === undefined || BOT_TOKEN === '') {
+  throw new Error('BOT_TOKEN is required')
+}
+
+const bot = new Telegraf(BOT_TOKEN)
+
+bot.telegram.getWebhookInfo().then(({url}) => {
+  if (url !== WEBHOOK_URL) {
+    bot.telegram.setWebhook(WEBHOOK_URL)
+  }
+}).catch(err => {
+  console.log({err})
+})
+
+bot.command('hello', (ctx) => {
+  ctx.reply('Hello, friend!')
+})
 
 app.get('/', async (req, res) => {
-  res.send('Your token is ' + BOT_TOKEN)
+  return res.send({ version })
 })
 
 app.post('/telegraf', async (req, res) => {
-  res.send({message: 'OK'})
+  try {
+    await bot.handleUpdate(req.body)
+  } finally {
+    return res.status(200).end()
+  }
 })
 
 exports.dolladollabillbot = app
