fb1a50ca29018993fd8dd6996b6b424fae819a72
Mauricio Kruijer <mauricio.kruijer@gmail.com>
2022-05-28 17:17:50 +0200

    Added puppeteer and simple test
    Needed to increase memory for cloud function

--- packages/apps/get-chart-url/src/main.ts
+++ packages/apps/get-chart-url/src/main.ts
@@ -1,4 +1,27 @@
-import { http } from '@google-cloud/functions-framework'
 import 'tslib'
+import { http } from '@google-cloud/functions-framework'
+import * as puppeteer from 'puppeteer'
 
-http('get-chart-url', (req, res) => res.send('ok!'))
+const getChartUrl = async () => {
+  const browser = await puppeteer.launch({
+    headless: true,
+    defaultViewport: {
+      width: 1920,
+      height: 1080
+    }
+  })
+
+  try {
+    const page = await browser.newPage();
+    await page.goto('https://www.github.com')
+    await page.waitForSelector('img.home-astro-mona.width-full')
+
+    const url = await page.$eval('img.home-astro-mona.width-full', (image: HTMLImageElement) => image.src)
+
+    return url
+  } catch (e) {
+    throw e;
+  }
+}
+
+http('get-chart-url',async (req, res) => res.send('ok! ' + await getChartUrl()))
