5d17f1d2be918b9fcedf8ba04c0e07bcbc9b9a5b
Mauricio Kruijer <mauricio.kruijer@gmail.com>
2022-05-28 23:04:04 +0200

    Save conversation in firestore on /hello cmd

--- packages/libs/store-conversation/src/lib/store-conversation.ts
+++ packages/libs/store-conversation/src/lib/store-conversation.ts
@@ -1,3 +1,13 @@
-export function storeConversation(): string {
-  return 'store-conversation!';
+import { Firestore } from '@google-cloud/firestore'
+
+const { PROJECT, GOOGLE_APPLICATION_CREDENTIALS } = process.env
+const db = new Firestore({ projectId: PROJECT, keyFile: GOOGLE_APPLICATION_CREDENTIALS })
+
+export async function storeConversation(conversation): Promise<void> {
+  const docRef = db.collection('conversations').doc(`${conversation.id}`)
+  const conversationExists = await docRef.get()
+
+  if (!conversationExists.exists) {
+    await docRef.set(conversation)
+  }
 }
